dedacdb9a32750c2701091de553252a8b57f01cd
Nathan Glover <nathan@glovers.id.au>
2023-03-18 05:12:42 +0000

    migrated to ipc2

--- greengrass-docker/src/robocat_pub.py
+++ greengrass-docker/src/robocat_pub.py
@@ -3,23 +3,15 @@ import logging
 import time
 import traceback
 
-import awsiot.greengrasscoreipc
+from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
 from awsiot.greengrasscoreipc.model import (
-    PublishToTopicRequest,
     PublishMessage,
     BinaryMessage
 )
 
-TIMEOUT = 10
-
-# setup logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-# setup ipc client
-ipc_client = awsiot.greengrasscoreipc.connect()
-
-# get environment variables
 thing_name = os.environ.get("AWS_IOT_THING_NAME")
 if not thing_name:
     logger.error("AWS_IOT_THING_NAME is empty")
@@ -30,25 +22,23 @@ if not hostname:
     logger.error("HOSTNAME is empty")
     raise ValueError("HOSTNAME is empty")
 
+
+def publish_binary_message_to_topic(ipc_client, topic, message):
+    binary_message = BinaryMessage(message=bytes(message, 'utf-8'))
+    publish_message = PublishMessage(binary_message=binary_message)
+    return ipc_client.publish_to_topic(topic=topic, publish_message=publish_message)
+
+
 while True:
     time.sleep(30)
-
     topic = "devopstar/robocat/{}/meow".format(thing_name)
-    message = "Meow! from {} running in {} container".format(thing_name, hostname)
-
-    request = PublishToTopicRequest()
-    request.topic = topic
-    publish_message = PublishMessage()
-    publish_message.binary_message = BinaryMessage()
-    publish_message.binary_message.message = bytes(message, "utf-8")
+    message = "Meow! from {} running in {} container".format(
+        thing_name, hostname)
 
     try:
-        request.publish_message = publish_message
-        operation = ipc_client.new_publish_to_topic()
-        logger.info("Publishing message to topic {}: {}".format(topic, message))
-        operation.activate(request)
-        future_response = operation.get_response()
-        future_response.result(TIMEOUT)
+        ipc_client = GreengrassCoreIPCClientV2()
+        publish_binary_message_to_topic(ipc_client, topic, message)
+        logger.info("Published message {} to topic {}".format(message, topic))
     except Exception as e:
-        traceback.print_exc()
         logger.error("Failed to publish message {}".format(e))
+        traceback.print_exc()
--- greengrass-docker/src/robocat_sub.py
+++ greengrass-docker/src/robocat_sub.py
@@ -3,61 +3,65 @@ import logging
 import time
 import traceback
 
-import awsiot.greengrasscoreipc
-import awsiot.greengrasscoreipc.client as client
+from awsiot.greengrasscoreipc.clientv2 import GreengrassCoreIPCClientV2
 from awsiot.greengrasscoreipc.model import (
-    SubscribeToTopicRequest,
-    SubscriptionResponseMessage
+    SubscriptionResponseMessage,
+    UnauthorizedError
 )
 
-TIMEOUT = 10
-
-# setup logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-# setup ipc client
-ipc_client = awsiot.greengrasscoreipc.connect()
-
-# get environment variables
 thing_name = os.environ.get("AWS_IOT_THING_NAME")
 if not thing_name:
     logger.error("AWS_IOT_THING_NAME is empty")
     raise ValueError("AWS_IOT_THING_NAME is empty")
 
-class StreamHandler(client.SubscribeToTopicStreamHandler):
-    def __init__(self):
-        super().__init__()
 
-    def on_stream_event(self, event: SubscriptionResponseMessage) -> None:
-        try:
-            message_string = str(event.binary_message.message, "utf-8")
-            logger.info("Received message from topic {}: {}".format(event.topic, message_string))
-        except:
-            traceback.print_exc()
-
-    def on_stream_error(self, error: Exception) -> bool:
-        # Handle error.
-        return True  # Return True to close stream, False to keep stream open.
-
-    def on_stream_closed(self) -> None:
-        # Handle close.
-        pass
+def on_stream_event(event: SubscriptionResponseMessage) -> None:
+    try:
+        message = str(event.binary_message.message, 'utf-8')
+        topic = event.binary_message.context.topic
+        logger.info('Received new message on topic %s: %s' % (topic, message))
+    except:
+        traceback.print_exc()
 
 
-topic = "devopstar/robocat/{}/meow".format(thing_name)
+def on_stream_error(error: Exception) -> bool:
+    logger.error('Subscribe to topic stream error: %s', error)
+    traceback.print_exc()
+    return False  # Return True to close stream, False to keep stream open.
 
-request = SubscribeToTopicRequest()
-request.topic = topic
-handler = StreamHandler()
-operation = ipc_client.new_subscribe_to_topic(handler) 
-operation.activate(request)
-future_response = operation.get_response()
-future_response.result(TIMEOUT)
+
+def on_stream_closed() -> None:
+    logger.info('Subscribe to topic stream closed.')
+
 
 # Keep the main thread alive, or the process will exit.
 while True:
-    time.sleep(10)
+    topic = "devopstar/robocat/{}/meow".format(thing_name)
 
-# To stop subscribing, close the operation stream.
-operation.close()
+    try:
+        ipc_client = GreengrassCoreIPCClientV2()
+        # Subscription operations return a tuple with the response and the operation.
+        _, operation = ipc_client.subscribe_to_topic(topic=topic, on_stream_event=on_stream_event,
+                                                     on_stream_error=on_stream_error, on_stream_closed=on_stream_closed)
+        logger.info("Subscribed to topic %s", topic)
+
+        # Keep the main thread alive, or the process will exit.
+        try:
+            while True:
+                time.sleep(10)
+        except InterruptedError:
+            logger.info("Interrupted, closing stream.")
+
+        # To stop subscribing, close the stream.
+        operation.close()
+    except UnauthorizedError as e:
+        logger.error("Failed to subscribe to topic %s: %s", topic, e)
+        traceback.print_exc()
+        exit(1)
+    except Exception as e:
+        logger.error("Failed to subscribe to topic %s: %s", topic, e)
+        traceback.print_exc()
+        exit(1)
