0314ecd32c106003b80fcf562d7f58ea22e16e29
Nathan Glover <nathan@glovers.id.au>
2023-06-17 12:02:07 +0800

    feature/greengrass-batch-uploader: Added basic deployment configuration for batch uploader PoC

---
+++ greengrass-batch-uploader/deployment.json.py
@@ -0,0 +1,106 @@
+import json
+import os
+import boto3
+
+"""
+export AWS_ACCOUNT_ID=$(aws sts get-caller-identity |  jq -r '.Account')
+export AWS_REGION=ap-southeast-2
+
+python deployment.json.py
+
+aws greengrassv2 create-deployment \
+    --output text \
+    --no-paginate \
+    --region ${AWS_REGION} \
+    --cli-input-json file://deployment.json
+"""
+
+AWS_REGION = os.getenv('AWS_REGION')
+if not AWS_REGION:
+    raise ValueError("AWS_REGION environment variable not set.")
+
+AWS_ACCOUNT_ID = os.getenv('AWS_ACCOUNT_ID')
+if not AWS_ACCOUNT_ID:
+    raise ValueError("AWS_ACCOUNT_ID environment variable not set.")
+
+LATEST_COMPONENT_VERSION = os.getenv('LATEST_COMPONENT_VERSION')
+if not LATEST_COMPONENT_VERSION:
+    print("LATEST_COMPONENT_VERSION environment variable not set. Fetching latest component version from AWS if possible.")
+
+
+def get_latest_component_version(component_name):
+    greengrassv2 = boto3.client('greengrassv2', region_name=AWS_REGION)
+    response = greengrassv2.list_component_versions(
+        arn=f"arn:aws:greengrass:{AWS_REGION}:{AWS_ACCOUNT_ID}:components:{component_name}"
+    )
+    return response['componentVersions'][0]['componentVersion']
+
+
+configuration = {
+    "targetArn": f"arn:aws:iot:{AWS_REGION}:{AWS_ACCOUNT_ID}:thinggroup/robocat",
+    "deploymentName": "Deployment for robocat group",
+    "components": {
+        "com.devopstar.Robocat": {
+            "componentVersion": LATEST_COMPONENT_VERSION or get_latest_component_version("com.devopstar.Robocat"),
+            "runWith": {},
+            "configurationUpdate": {
+                "merge": json.dumps({
+                    "Enabled": "true",
+                    "Frequency": "100"
+                })
+            }
+        },
+        "aws.greengrass.StreamManager": {
+            "componentVersion": "2.1.8",
+        },
+        "com.devopstar.json.gzip": {
+            "componentVersion": "1.0.3",
+            "configurationUpdate": {
+                "merge": json.dumps({
+                    "OutputFolder": "/tmp/greengrass/gzip"
+                })
+            },
+            "runWith": {}
+        },
+        "aws.greengrass.labs.s3.file.uploader": {
+            "componentVersion": "1.0.3",
+            "configurationUpdate": {
+                "merge": json.dumps({
+                    "BucketName": "greengrass-stream-manager-gzip-ap-southeast-2-536829251200",
+                    "PathName": "/tmp/greengrass/gzip/*",
+                    "Interval": "10",
+                })
+            },
+            "runWith": {}
+        },
+        "aws.greengrass.Nucleus": {
+            "componentVersion": "2.10.2"
+        },
+        "aws.greengrass.Cli": {
+            "componentVersion": "2.10.2"
+        },
+        "aws.greengrass.LocalDebugConsole": {
+            "componentVersion": "2.3.0",
+            "configurationUpdate": {
+                "merge": json.dumps({
+                    "httpsEnabled": "false"
+                })
+            },
+            "runWith": {}
+        }
+    },
+    "deploymentPolicies": {
+        "failureHandlingPolicy": "ROLLBACK",
+        "componentUpdatePolicy": {
+            "timeoutInSeconds": 60,
+            "action": "NOTIFY_COMPONENTS"
+        },
+        "configurationValidationPolicy": {
+            "timeoutInSeconds": 60
+        }
+    },
+    "iotJobConfiguration": {}
+}
+
+with open('deployment.json', 'w') as json_file:
+    json.dump(configuration, json_file, indent=4)
---
+++ greengrass-batch-uploader/src/robocat.py
@@ -0,0 +1,63 @@
+import asyncio
+import time
+import logging
+import random
+import json
+import sys
+
+from datetime import datetime
+
+from greengrasssdk.stream_manager import (
+    StreamManagerClient,
+    StreamManagerException
+)
+
+steam_name = "BatchMessageStream"
+stream_manager_client = StreamManagerClient()
+
+logging.basicConfig(level=logging.INFO)
+
+speed = 50
+temperature = 25
+location = {'lat': -31.976056, 'lng': 115.9113084}
+
+
+def generate_random_json(id, speed, temperature, location):
+    speed += random.randint(-5, 5)
+    temperature = round(temperature + random.uniform(-0.5, 0.5), 2)
+    location['lat'] += random.uniform(-0.0001, 0.0001)
+    location['lng'] += random.uniform(-0.0001, 0.0001)
+    return json.dumps({
+        "id": id,
+        "timestamp": datetime.now().isoformat(),
+        "speed": speed,
+        "temperature": temperature,
+        "location": location
+    }).encode(), speed, temperature, location
+
+
+if __name__ == "__main__":
+    # args : enabled, frequency
+    if len(sys.argv) == 3:
+        enabled = bool(sys.argv[1])
+        frequency = int(sys.argv[2])
+
+        try:
+            while True:
+                if enabled:
+                    message, speed, temperature, location = generate_random_json("1", speed, temperature, location)
+                    try:
+                        sequence_number = stream_manager_client.append_message(
+                            stream_name=steam_name, data=message)
+                        logging.info(
+                            f"Message published to Stream Manager: {steam_name} - sequence number: {sequence_number}")
+                    except StreamManagerException:
+                        logging.error("StreamManagerException occurred", exc_info=True)
+                    except (ConnectionError, asyncio.TimeoutError):
+                        logging.error(
+                            "Connection or Timeout error occurred", exc_info=True)
+                time.sleep(frequency)
+        except:
+            logging.error("Exception occurred", exc_info=True)
+    else:
+        logging.error(f'2 argument required, only {len(sys.argv)-1} provided.')
