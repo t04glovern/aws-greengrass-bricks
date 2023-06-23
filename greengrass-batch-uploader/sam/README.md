1c8305ed5f0d3802da6fbfc712a47bfc5f1920da
Nathan Glover <nathan@glovers.id.au>
2023-06-23 04:31:17 +0000

    feature/batch-uploader PoC complete with athena iceberg

--- greengrass-batch-uploader/sam/README.md
+++ greengrass-batch-uploader/sam/README.md
@@ -1,7 +1,34 @@
 # greengrass-batch-uploader
 
+## Deploy
+
 ```bash
 pip3 install aws-sam-cli==1.87.0
 sam build
 sam deploy
 ```
+
+## Relies on
+
+1. An Athena table called `greengrass_data_iceberg` with the following schema:
+
+    ```sql
+    CREATE TABLE IF NOT EXISTS greengrass_data_iceberg (
+        `id` string,
+        `timestamp` timestamp,
+        `speed` int,
+        `temperature` float,
+        `location` struct<lat:float, lng:float>
+    )
+    PARTITIONED BY (hour(`timestamp`))
+    LOCATION 's3://greengrass-stream-manager-iceberg-ap-southeast-2-536829251200/'
+    TBLPROPERTIES (
+        'table_type'='ICEBERG',
+        'format'='parquet',
+        'write_compression'='gzip',
+        'vacuum_min_snapshots_to_keep'='5',
+        'vacuum_max_snapshot_age_seconds'='86400'
+    );
+    ``
+
+2. An Athena Query output location of `s3://athena-ap-southeast-2-536829251200/` attached to the `primary` workgroup.
--- greengrass-batch-uploader/sam/src/index.py
+++ greengrass-batch-uploader/sam/src/index.py
@@ -1,49 +1,129 @@
 import boto3
 import json
 import os
+import time
 
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.utilities.data_classes import event_source, SQSEvent
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
-logger = Logger()
+event_logger = Logger()
+
+ATHENA_DATABASE = os.environ.get('ATHENA_DATABASE', 'default')
+ATHENA_TABLE = os.environ.get('ATHENA_TABLE')
+ATHENA_WORKGROUP = os.environ.get('ATHENA_WORKGROUP', 'primary')
+BATCH_BUCKET_NAME = os.environ.get('BATCH_BUCKET_NAME')
 
 
-def copy_s3_object(source_bucket, source_key, destination_bucket, destination_key):
-    s3 = boto3.client('s3')
+def run_athena_query(query):
+    athena_client = boto3.client('athena')
 
     try:
-        s3.copy_object(Bucket=destination_bucket,
-                       Key=destination_key,
-                       CopySource={'Bucket': source_bucket, 'Key': source_key})
+        event_logger.info(f"Executing query: {query}")
 
-        logger.info(
-            f"Copied object {source_bucket}/{source_key} to {destination_bucket}/{destination_key}")
+        query_start_response = athena_client.start_query_execution(
+            QueryString=query,
+            QueryExecutionContext={"Database": ATHENA_DATABASE},
+            WorkGroup=ATHENA_WORKGROUP,
+        )
 
-    except Exception as e:
-        logger.exception(f"Error copying S3 object: {e}")
+        query_execution_id = query_start_response["QueryExecutionId"]
+
+        while True:
+            query_status_response = athena_client.get_query_execution(
+                QueryExecutionId=query_execution_id
+            )
+
+            query_execution_status = query_status_response["QueryExecution"]["Status"]["State"]
+
+            if query_execution_status in ["SUCCEEDED"]:
+                event_logger.info(f"Query executed successfully.")
+                return
+            elif query_execution_status in ["FAILED", "CANCELLED"]:
+                event_logger.error(
+                    f"Query execution failed. Status: {query_execution_status}")
+                raise Exception(
+                    f"Query execution failed. Status: {query_execution_status}")
+            else:
+                event_logger.info(
+                    f"Query execution in progress. Current status: {query_execution_status}")
+
+            time.sleep(5)
+
+    except Exception as err:
+        event_logger.exception(f"Error during query execution: {err}")
+        raise err
 
 
-@logger.inject_lambda_context
+def transfer_s3_file(src_bucket, src_key, dest_bucket, dest_key):
+    s3_client = boto3.client('s3')
+
+    try:
+        s3_client.copy_object(Bucket=dest_bucket,
+                              Key=dest_key,
+                              CopySource={'Bucket': src_bucket, 'Key': src_key})
+
+        event_logger.info(
+            f"Transferred file {src_bucket}/{src_key} to {dest_bucket}/{dest_key}")
+
+    except Exception as err:
+        event_logger.exception(f"Error transferring S3 file: {err}")
+
+
+@event_logger.inject_lambda_context
 @event_source(data_class=SQSEvent)
 def handler(event: SQSEvent, context: LambdaContext):
-    logger.info(f"Event received: {event}")
-
     for record in event.records:
         try:
-            message = json.loads(record.body)
+            msg = json.loads(record.body)
 
-            source_bucket = message['detail']['bucket']['name']
-            source_key = message['detail']['object']['key']
+            src_bucket = msg['detail']['bucket']['name']
+            src_key = msg['detail']['object']['key']
 
-            logger.info(
-                f"Processing {source_key} from bucket {source_bucket}, Starting to copy object to temporary location")
+            event_logger.info(
+                f"Processing {src_key} from bucket {src_bucket}, Starting to transfer file to temporary location")
 
-            destination_bucket = os.environ.get('BATCH_BUCKET_NAME')
-            destination_key = f"{context.aws_request_id}/{source_key.split('/')[-1]}"
+            dest_bucket = BATCH_BUCKET_NAME
+            dest_key = f"{context.aws_request_id}/{src_key.split('/')[-1]}"
 
-            copy_s3_object(source_bucket, source_key,
-                           destination_bucket, destination_key)
+            transfer_s3_file(src_bucket, src_key, dest_bucket, dest_key)
 
-        except Exception as e:
-            logger.exception(f"Error processing record: {e}")
+            table_prefix = '/'.join(dest_key.split('/')[:-1])
+            tmp_table_id = table_prefix.split('-')[-1].lower()
+
+            ATHENA_TMP_TABLE = "{}_{}".format(ATHENA_TABLE, tmp_table_id)
+            ATHENA_TMP_S3_BUCKET = dest_bucket
+            ATHENA_TMP_TABLE_PREFIX = table_prefix
+
+            # Create Temporary Table
+            run_athena_query(f"""
+            CREATE EXTERNAL TABLE IF NOT EXISTS {ATHENA_DATABASE}.{ATHENA_TMP_TABLE} (
+                `id` string,
+                `timestamp` timestamp,
+                `speed` int,
+                `temperature` float,
+                `location` struct<lat:float, lng:float>
+            )
+            ROW FORMAT SERDE 'org.apache.hive.hcatalog.data.JsonSerDe'
+            WITH SERDEPROPERTIES ( "timestamp.formats"="yyyy-MM-dd'T'HH:mm:ss.SSSSSS" )
+            LOCATION 's3://{ATHENA_TMP_S3_BUCKET}/{ATHENA_TMP_TABLE_PREFIX}'
+            TBLPROPERTIES ('has_encrypted_data'='false')
+            """)
+
+            # Insert into Main Table
+            run_athena_query(
+                f"INSERT INTO {ATHENA_DATABASE}.{ATHENA_TABLE} SELECT * FROM {ATHENA_DATABASE}.{ATHENA_TMP_TABLE};")
+
+            # Drop Temporary Table
+            run_athena_query(
+                f"DROP TABLE {ATHENA_DATABASE}.{ATHENA_TMP_TABLE};")
+
+        except Exception as err:
+            event_logger.exception(f"Error processing record: {err}")
+            try:
+                run_athena_query(
+                    f"DROP TABLE {ATHENA_DATABASE}.{ATHENA_TMP_TABLE};")
+            except Exception as e:
+                event_logger.exception(f"Error dropping temporary table: {e}")
+                raise e
+            raise err
