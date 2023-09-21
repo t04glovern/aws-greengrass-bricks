9448e7357654812c3312dffae3a0597fd5c50326
Nathan Glover <nathan@glovers.id.au>
2023-09-21 15:11:52 +0000

    bumped com.devopstar.S3Ingestor 1.0.4

--- greengrass-batch-uploader/deployment.json.py
+++ greengrass-batch-uploader/deployment.json.py
@@ -51,7 +51,7 @@ configuration = {
             }
         },
         "com.devopstar.S3Ingestor": {
-            "componentVersion": "1.0.3",
+            "componentVersion": "1.0.4",
             "configurationUpdate": {
                 "merge": json.dumps({
                     "Uploader": {
